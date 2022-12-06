1dd4d35bd1a329380d0ffcdf1b828343350d5560
Mardian2507 <mardian.2507@gmail.com>
2022-12-06 09:27:41 +0700

    add progres bar

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="w-full bg-emerald-600 text-center">
-      <h1 className="text-2xl">Footer</h1>
-    </div>
+    <footer className="w-full">
+      <h1>footer</h1>
+    </footer>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <NextNProgress />
       <Component {...pageProps} />
     </ThemeProvider>
   );
