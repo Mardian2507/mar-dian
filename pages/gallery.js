9f70787cf2676f84a46e59b42089c3150eb970c2
Mardian2507 <mardian.2507@gmail.com>
2022-12-12 16:20:08 +0700

    p

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,10 +1,45 @@
 import Layout from "@/components/Layout";
+import Image from "next/image";
 
 export default function Gallery() {
   return (
-    <Layout title="About Mardian">
-      <div id="gallery" className="min-h-screen">
-        <h1 className="text-3xl font-bold underline pt-20">page Gallery</h1>
+    <Layout title="Gallery Mardian">
+      <div id="gallery" className="relative h-[1500px] inset-y-20">
+        <Image
+          alt="bg1"
+          src="/assets/img/bg.png"
+          width="3000"
+          height="3000"
+          className="bg-cover w-full absolute"
+        />
+        <Image
+          alt="bgStone"
+          src="/assets/img/bgstone.png"
+          width="3000"
+          height="3000"
+          className="bg-cover w-full absolute"
+        />
+        <Image
+          alt="bird"
+          src="/assets/img/bird.png"
+          width="3000"
+          height="3000"
+          className="bg-cover w-full absolute"
+        />
+        <Image
+          alt="me"
+          src="/assets/img/me.png"
+          width="3000"
+          height="3000"
+          className="bg-cover w-full absolute"
+        />
+        <Image
+          alt="my"
+          src="/assets/img/my.png"
+          width="3000"
+          height="3000"
+          className="bg-cover w-full absolute"
+        />
       </div>
     </Layout>
   );
