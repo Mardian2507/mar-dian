58af460c54d2918fff4e77583f57f86a5a84cd86
Mardian2507 <mardian.2507@gmail.com>
2022-12-20 23:44:42 +0700

    gallery mod

--- components/PostCaption.js
+++ components/PostCaption.js
@@ -6,13 +6,17 @@ export default function PostCaption({
 }) {
   return (
     <>
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center text-center py-1 flex-wrap space-x-1 text-[6px] md:text-[10px]">
         <div className="uppercase">{places}</div>
         <span>&bull;</span>
         <div>{date}</div>
       </div>
-      <h2>{postTitle}</h2>
-      <div>{shortDescription}</div>
+      <span className="flex justify-center text-base md:text-lg font-semibold">
+        {postTitle}
+      </span>
+      <div className="text-sm text-secondary dark:text-light">
+        {shortDescription}
+      </div>
     </>
   );
 }
--- pages/gallery.js
+++ pages/gallery.js
@@ -11,91 +11,19 @@ import Github from "@public/assets/skills/github1.png";
 import NextJS from "@public/assets/skills/nextjs.png";
 import CardPost from "components/CardPost";
 import { useState } from "react";
+import mockPosts from "../utils/posts.json";
 
 export default function Gallery() {
-  const [posts, setPost] = useState([
-    {
-      id: 1,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 2,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 3,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 4,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 5,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 6,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 7,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 8,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-    {
-      id: 9,
-      thumbnail: "/assets/mar-dian.webp",
-      places: "Bandung",
-      date: "25/07/2023",
-      postTitle: "backpak judul",
-      shortDescription: "ini deskripsi",
-    },
-  ]);
+  const [posts, setPost] = useState(mockPosts);
 
   return (
     <Layout title="Gallery Mardian">
-      <div className="w-full h-full justify-center items-center ">
-        <div>
+      <div className="w-full h-full">
+        <div className="w-full">
           <ParallaxHero />
         </div>
 
-        <div className="container px-5">
+        <div className="container px-5 flex flex-col justify-center items-center">
           <h2
             className="sticky z-10 top-2 pt-2 text-center tracking-widest  text-primary backdrop-blur rounded-lg uppercase"
             data-aos="fade-down"
@@ -104,127 +32,15 @@ export default function Gallery() {
             Gallery
           </h2>
 
-          <div className="grid grid-cols-4">
+          <div className="grid grid-cols-4 gap-1 md:grid-cols-5 md:gap-2 mt-10">
             {posts.map((post) => (
-              <div key={post.id}>
+              <div
+                key={post.id}
+                className="p-1 shadow-xl rounded-xl hover:scale-105 ease-in duration-300"
+              >
                 <CardPost {...post} />
               </div>
             ))}
-          </div>
-
-          <div className="grid grid-cols-3 gap-1 lg:grid-cols-4 md:gap-8">
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={Html} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>HTML</h3>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={Css} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>CSS</h3>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={Javascript} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>JavaScript</h3>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={ReactImg} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>React</h3>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={Tailwind} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>Tailwind</h3>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={Firebase} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>Firebase</h3>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={Github} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>Github</h3>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 md:p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-                <div
-                  className="m-auto"
-                  data-aos="flip-up"
-                  data-aos-duration="2000"
-                >
-                  <Image src={NextJS} width="64px" height="64px" alt="/" />
-                </div>
-                <div className="flex flex-col items-center justify-center">
-                  <h3>Next</h3>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
